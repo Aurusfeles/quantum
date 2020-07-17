d8603594bf8909ec0d6b848c60fc6509b3d109db
Aurus <aurusfeles@sfr.fr>
2020-07-17 12:40:07 +0200

    QuantumCard component created

---
+++ components/QuantumCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="bg-local rounded overflow-hidden border border-gray-400 shadow-md mt-8 ml-8 max-w-3xl p-4 p-8"
+    :style="{'background-image': image_path}"
+  >
+    <div class="md:flex">
+      <div class="mt-4 md:mt-0 md:ml-6">
+        <div
+          class="uppercase tracking-wide text-sm text-indigo-600 font-bold"
+          :class="card_info[language]['status']"
+        >{{card_info[language]['name']}}</div>
+        <div
+          class="block mt-1 text-lg leading-tight font-semibold text-gray-800"
+        >{{card_info[language]['text']}}</div>
+        <p class="mt-2 text-gray-500 italic">{{card_info[language]['notes']}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    card_info: Object,
+    language: String
+  },
+  computed: {
+    image_path: function() {
+      return "~/static/images/" + this.card_info.art + ".png";
+    }
+  }
+};
+</script>
--- pages/skills.vue
+++ pages/skills.vue
@@ -1,23 +1,11 @@
 <template>
   <div>
-    <div
-      class="bg-white rounded overflow-hidden border border-gray-400 shadow-md mt-8 ml-8 max-w-3xl p-4 p-8"
+    <QuantumCard
       v-for="(skill,index) in skills"
       :key="index"
-    >
-      <div class="md:flex">
-        <div class="mt-4 md:mt-0 md:ml-6">
-          <div
-            class="uppercase tracking-wide text-sm text-indigo-600 font-bold"
-            :class="skill[language]['status']"
-          >{{skill[language]['name']}}</div>
-          <div
-            class="block mt-1 text-lg leading-tight font-semibold text-gray-800"
-          >{{skill[language]['text']}}</div>
-          <p class="mt-2 text-gray-500 italic">{{skill[language]['notes']}}</p>
-        </div>
-      </div>
-    </div>
+      :card_info="skill"
+      :language="language"
+    ></QuantumCard>
   </div>
 </template>
 
